fbb34b1a0664f4479b7d0b7d63d538cd71dced3a
Mike Valeriano <mkvlrn@proton.me>
2025-01-20 08:49:46 -0300

    fix: add update info in template list

--- src/lib/prompts/project-type.ts
+++ src/lib/prompts/project-type.ts
@@ -4,10 +4,23 @@ import type { GithubRepoResponse } from "~/types";
 
 export function promptProjectType(templateList: GithubRepoResponse[]) {
   return prompts.select({
+    pageSize: 10,
+    loop: false,
     message: colorette.dim(colorette.yellow("Project type")),
     choices: templateList.map((template) => ({
-      name: `${template.name.split("-").pop() ?? "Unknown"} (${template.description})`,
+      name: formatSelectName(template),
       value: template.name,
     })),
   });
+}
+
+function formatSelectName(template: GithubRepoResponse) {
+  const updatedDaysAgo =
+    (Date.now() - new Date(template.updated_at).getTime()) / (1000 * 60 * 60 * 24);
+
+  let name = template.name.split("-").pop() ?? "Unknown";
+  name += ` - ${template.description}`;
+  name += `\n    updated ${updatedDaysAgo.toFixed(0)} days ago`;
+
+  return name;
 }
--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,8 @@ export interface GithubRepoResponse {
   description: string;
   // biome-ignore lint/style/useNamingConvention: just obeying github api format
   is_template: boolean;
+  // biome-ignore lint/style/useNamingConvention: just obeying github api format
+  updated_at: string;
 }
 
 export const packageManagers = ["npm", "yarn", "pnpm", "bun"] as const;
